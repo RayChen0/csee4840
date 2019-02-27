e34ce6c4c8529a37260a072a6f142d0dfc8d081a
sfw006033 <47928783+sfw006033@users.noreply.github.com>
2019-02-26 22:25:19 -0500

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -134,6 +134,9 @@ int main()
          if (packet.keycode[1] == 00){/* we only read in when only one character key is pressed to prevent shack */
           dispCharacter = keyValue(packet.modifiers, packet.keycode[0]);
          }
+	 if (packet.keycode[1] != 00){/* we only read in when only one character key is pressed to prevent shack */s
+	  dispCharacter = keyValue(packet.modifiers, packet.keycode[1]);
+         }
          else{
            continue;
          }
