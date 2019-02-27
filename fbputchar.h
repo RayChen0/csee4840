7c8bdfd9ac0af6aaacab3dd0426c2d46d873b638
sfw006033 <47928783+sfw006033@users.noreply.github.com>
2019-02-26 21:38:25 -0500

    Update fbputchar.h

--- fbputchar.h
+++ fbputchar.h
@@ -122,6 +122,5 @@
 extern int fbopen(void);
 extern void fbputchar(char, int, int);
 extern void fbputs(const char *, int, int);
-extern void fbputcursor(const char *, int, int);
 extern char keyValue(int, int);
 #endif
